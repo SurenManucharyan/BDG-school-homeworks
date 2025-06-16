18e6232383a1ad7c87449a88533176541814cc09
SurenManucharyan <43904797+SurenManucharyan@users.noreply.github.com>
2025-06-16 19:55:34 +0400

    homework 11 changes

--- JS-Homework-11/homework11.js
+++ JS-Homework-11/homework11.js
@@ -9,19 +9,15 @@ async function infoFromLink(urls) {
       return Promise.all(res.map((response) => response.json()));
     })
     .then((values) => {
-      console.log(values[0], values[1].data);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-
-  Promise.all(urls.map((url) => fetch(url)))
-    .then((res) => {
-      return Promise.all(res.map((response) => response.json()));
-    })
-    .then((values) => {
       console.log(
-        values[0].filter((country) => country.region === "Europe"),
+        values[0]
+          .filter((country) => country.region === "Europe")
+          .map((country) => ({
+            name: country.name.common,
+            population: country.population,
+            region: country.region,
+          }))
+          .sort((a, b) => b.population - a.population),
         values[1].data.filter((el) => el.length > 50)
       );
     })
